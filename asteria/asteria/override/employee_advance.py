75dbeed270fb4f8a8e39b65f1e0b427788d4ab9d
viralkansodiya <viral@fosserp.com>
2026-03-10 05:15:26 +0000

    fix: enhance employee advance status update and handle missing work_order in stock reconciliation

--- asteria/asteria/override/employee_advance.py
+++ asteria/asteria/override/employee_advance.py
@@ -1,9 +1,10 @@
 import frappe
 from hrms.hr.doctype.employee_advance.employee_advance import EmployeeAdvance
 
-class CustomEmployeeAdvance(EmployeeAdvance):
-    def set_status(self, update=False):
-        super().set_status(update)
-        self.custom_status = self.status
 
-        
+class CustomEmployeeAdvance(EmployeeAdvance):
+	def set_status(self, update=False):
+		super().set_status(update)
+		self.custom_status = self.status
+		if update and frappe.db.has_column("Employee Advance", "custom_status"):
+			self.db_set("custom_status", self.status)
